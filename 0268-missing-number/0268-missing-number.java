39c380a5c18379fcfae0620030c44499d74ee3fa
princeraj9204 <princeraj887362@gmail.com>
2026-09-12 17:04:23 +0530

    Time: 0 ms (100%), Space: 47.3 MB (65.94%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int len =  nums.length;
-        Arrays.sort(nums);
-        if(nums[len-1]<len){
-            return len;
+        int n = nums.length;
+        int freq[] = new int[n+1];
+        for(int i=1;i<n+1;i++){
+            freq[nums[i-1]]++;
         }
-        int i=0;
-        int m = 0;
-        int j=0;
-        while(j<len){
-            if(nums[j]!=i){
-                m = i;
-                break;
+        for(int i=0;i<n+1;i++){
+            if(freq[i] == 0){
+                return i;
             }
-            i++;
-            j++;
         }
-        return m;
+        return 0;
     }
 }
